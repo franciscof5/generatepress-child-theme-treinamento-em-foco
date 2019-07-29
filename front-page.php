4049d1d5e1b583cf70fa915c6c3a8a33ec4fbf3f
Francisco Mat <francisco@f5sites.com>
2019-07-28 23:50:04 -0300

    bullet

--- front-page.php
+++ front-page.php
@@ -29,7 +29,7 @@ wp_enqueue_style('bootstrap-css', get_bloginfo("stylesheet_directory")."/assets/
 		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
 		<li data-target="#carousel-example-generic" data-slide-to="2"></li>
 		<li data-target="#carousel-example-generic" data-slide-to="3"></li>
-		
+		<li data-target="#carousel-example-generic" data-slide-to="4"></li>
 	</ol>
 
 	<!-- Wrapper for slides -->
